c1f3f157d5bb4cde2916fd5d00f841fa545374f5
vishnu <vjp5266@psu.edu>
2021-12-30 17:11:04 -0800

    Problem solving

---
+++ Tree/Inorder-traversal.py
@@ -0,0 +1,17 @@
+# Give the root of a binary tree, return the list of inorder traversal of its node values
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root) :
+        res = []
+
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.val)
+            res = res + self.inorderTraversal(root.right)
+        return res
